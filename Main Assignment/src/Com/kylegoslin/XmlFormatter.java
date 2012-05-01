dd09fc8edbd2993e416fc16c3affcea7f63b4847
Tomasz Piechota <piechota.tomasz@gmail.com>
2012-05-01 13:23:59 +0000

    Create StringBuffer

--- Main Assignment/src/Com/kylegoslin/XmlFormatter.java
+++ Main Assignment/src/Com/kylegoslin/XmlFormatter.java
@@ -1,5 +1,18 @@
 package Com.kylegoslin;
 
-public class XmlFormatter {
+import java.util.logging.LogRecord;
+import java.util.logging.XMLFormatter;
+
+public class XmlFormatter extends XMLFormatter {
+	
+	// Create StringBuffer
+	StringBuffer sb = new StringBuffer();
+	
+	
+	public String format(LogRecord record) {
+		
+		return null;
+				
+	}
 
 }
