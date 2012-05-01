4469db3ea79c2e8aa71672c266a924057c89a7f7
Tomasz Piechota <piechota.tomasz@gmail.com>
2012-05-01 22:07:08 +0000

    Update on StartUp

--- Main Assignment/src/Com/kylegoslin/StartUp.java
+++ Main Assignment/src/Com/kylegoslin/StartUp.java
@@ -6,32 +6,29 @@ import java.util.logging.Level;
 public class StartUp {
 	
 	// Variables
-	private static String className;
-	private static String packageName;
-
+	static String className;
+	static String packageName;
+	
 	// This method takes class name and package name from testing class
 	public static void config(String cn, String pn){
 		
 		className = cn;
 		packageName = pn;
 		
-		//Test to be removed when finished
+		//Test printout - to be removed when finished
 		System.out.println(className);
 		System.out.println(packageName);
 		
-		
 		try {
 			
-			LogHandler.Configure();
+			LogHandler.Config();
 			
 		} catch (IOException e) {
 			
 			e.printStackTrace();
 			
 			throw new RuntimeException("File could not be created!");
-			
 		}
-		
 		
 		// Set logger level and messages for each level
 		LogHandler.logger.setLevel(Level.ALL);
@@ -41,13 +38,12 @@ public class StartUp {
 		
 	}
 	
-	
 	// Getters and Setters
 	public String getClassName() {
 		return className;
 	}
 
-	public static void setClassName(String className) {
+	public void setClassName(String className) {
 		StartUp.className = className;
 	}
 
@@ -58,5 +54,69 @@ public class StartUp {
 	public static void setPackageName(String packageName) {
 		StartUp.packageName = packageName;
 	}
-	
+
 }
+
+
+//package Com.kylegoslin;
+//
+//import java.io.IOException;
+//import java.util.logging.Level;
+//
+//public class StartUp {
+//	
+//	// Variables
+//	static String className;
+//	static String packageName;
+//
+//	// This method takes class name and package name from testing class
+//	public static void config(String cn, String pn){
+//		
+//		className = cn;
+//		packageName = pn;
+//		
+//		//Test to be removed when finished
+//		System.out.println(className);
+//		System.out.println(packageName);
+//		
+//		
+//		try {
+//			
+//			LogHandler.Configure();
+//			
+//		} catch (IOException e) {
+//			
+//			e.printStackTrace();
+//			
+//			throw new RuntimeException("File could not be created!");
+//			
+//		}
+//		
+//		
+//		// Set logger level and messages for each level
+//		LogHandler.logger.setLevel(Level.ALL);
+//		LogHandler.logger.severe("Severe Error Message");
+//		LogHandler.logger.warning("Warning Message");
+//		LogHandler.logger.info("Error Message");
+//		
+//	}
+//	
+//	
+//	// Getters and Setters
+//	public String getClassName() {
+//		return className;
+//	}
+//
+//	public static void setClassName(String className) {
+//		StartUp.className = className;
+//	}
+//
+//	public String getPackageName() {
+//		return packageName;
+//	}
+//
+//	public static void setPackageName(String packageName) {
+//		StartUp.packageName = packageName;
+//	}
+//	
+//}
