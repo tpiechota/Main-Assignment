dde581ae16400f34b25f7ba94627910fb1acd3bf
Tomasz Piechota <piechota.tomasz@gmail.com>
2012-05-01 22:19:03 +0000

    Add example method to ExampleUsage class

--- Main Assignment/src/Com/kylegoslin/ExampleUsage.java
+++ Main Assignment/src/Com/kylegoslin/ExampleUsage.java
@@ -4,9 +4,8 @@ public class ExampleUsage {
 	
 	/***
 	 * 
-	 * 
 	 * Author: Tomasz Piechota 
-	 * 
+	 * Project: Java - Semester 4 - Main Assignment
 	 * 
 	 ***/
 	
@@ -17,11 +16,15 @@ public class ExampleUsage {
 		StartUp.config(this.getClass().getName().toString(), this.getClass().getPackage().getName().toString());
 		/*** Logger StartUp - END ***/
 		
-		dupa();
+		exampleMethod();
 		
 	}
 	
-	public void dupa(){}
+	public void exampleMethod(){
+		
+		System.out.println("Hello World!");
+		
+	}
 	
 	public static void main (String [] args){
 		
