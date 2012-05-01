5736f939aaf49635c4ac3848ac60ff742d8e99bb
Tomasz Piechota <piechota.tomasz@gmail.com>
2012-05-01 15:55:34 +0000

    Basic XmlFormatter Setup

--- Main Assignment/src/Com/kylegoslin/XmlFormatter.java
+++ Main Assignment/src/Com/kylegoslin/XmlFormatter.java
@@ -1,5 +1,6 @@
 package Com.kylegoslin;
 
+import java.lang.reflect.Method;
 import java.util.logging.LogRecord;
 import java.util.logging.XMLFormatter;
 
@@ -8,8 +9,41 @@ public class XmlFormatter extends XMLFormatter {
 	// Create StringBuffer
 	StringBuffer sb = new StringBuffer();
 	
+	// Initialize bugID
+	private int bugID;
 	
 	public String format(LogRecord record) {
+		
+		try {			
+			
+			// Get methods from testing class using Reflections
+			Class<?> myClass = Class.forName("ExampleUsage");
+			Method m[] = myClass.getDeclaredMethods();
+			
+			bugID = 0;
+			
+			for(int i = 0; i < m.length; i++){				
+				
+				sb.append("<record>\n");
+				sb.append("<bugID>" + bugID + "</bugID>\n");
+				sb.append("<message>" + bugID + "</message>\n");
+				sb.append("<Time_Epoch)>" + bugID + "</Time_Epoch>\n");				
+				sb.append("<logger>" + bugID + "</logger>\n");
+				sb.append("<messsageType>" + bugID + "</messsageType>\n");
+				sb.append("<class>" + bugID + "</class>\n");
+				sb.append("<package>" + bugID + "</package>");
+				sb.append("<method>" + bugID + "</method>\n");
+				sb.append("</record>\n");
+				
+				bugID++;
+		 		   
+		 	}		
+			
+		} catch (ClassNotFoundException e){
+			
+			e.printStackTrace();
+			
+		}
 		
 		return null;
 				
