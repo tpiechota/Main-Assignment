5678428707675764cbbc45e6ff1396953a652cdf
Tomasz Piechota <piechota.tomasz@gmail.com>
2012-05-01 16:25:40 +0000

    Initialize variables that will store testing class name and package name

--- Main Assignment/src/Com/kylegoslin/StartUp.java
+++ Main Assignment/src/Com/kylegoslin/StartUp.java
@@ -1,5 +1,21 @@
 package Com.kylegoslin;
 
 public class StartUp {
+	
+	// Variables
+	static String className;
+	static String packageName;
 
+	// This method takes class name and package name from testing class
+	public static void config(String cn, String pn){
+		
+		className = cn;
+		packageName = pn;
+		
+		//Test to be removed when finished
+		System.out.println(className);
+		System.out.println(packageName);
+		
+	}
+	
 }
