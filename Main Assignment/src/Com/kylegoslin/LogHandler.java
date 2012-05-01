d4cdeb8b6d1ac472f185938fdd3c51652c0d0c7d
Tomasz Piechota <piechota.tomasz@gmail.com>
2012-05-01 15:13:00 +0000

    Create variable timeNow which holds current time and date (converted from epoch milliseconds)

--- Main Assignment/src/Com/kylegoslin/LogHandler.java
+++ Main Assignment/src/Com/kylegoslin/LogHandler.java
@@ -1,6 +1,8 @@
 package Com.kylegoslin;
 
 import java.io.IOException;
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -9,25 +11,33 @@ import java.util.logging.XMLFormatter;
 
 public class LogHandler {
 	
-	//Create logger
+	// Create logger
 	static Logger logger = Logger.getLogger("Com.kylegoslin");
 	
-	//Initialize FileHandlers
+	// Initialize FileHandlers
 	static private FileHandler txtFile;
 	static private FileHandler xmlFile;
 	
-	//Initialize Formatters
+	// Initialize Formatters
 	static private SimpleFormatter txtFormatter;
 	static private XMLFormatter xmlFormatter;
+	
+	// Date and Time
+	static long nowEpoch = System.currentTimeMillis();
+	static Format nowFormatter = new SimpleDateFormat("ddMMyyyyHHmmssz");
+	static String timeNow;
 	
 	static public void Configure() throws IOException {
 		
 		// Set logger level (global)
 		logger.setLevel(Level.INFO);
 		
+		// Assign converted epoch time to timeNow variable 
+		timeNow = nowFormatter.format(nowEpoch);
+		
 		// Create FileHandlers
-		txtFile = new FileHandler("Log_file.txt");
-		xmlFile = new FileHandler("Log_file.xml");
+		txtFile = new FileHandler("Logging_" + timeNow + ".txt");
+		xmlFile = new FileHandler("Logging_" + timeNow + ".xml");
 		
 		// Create Formatters
 		txtFormatter = new SimpleFormatter();
