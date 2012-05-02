d6a58de9473a12edb0adb68359461feb50c35848
Tomasz Piechota <piechota.tomasz@gmail.com>
2012-05-02 07:57:56 +0000

    Name change

---
+++ Main Assignment/src/Com/kylegoslin/ExampleUsage_1.java
@@ -0,0 +1,35 @@
+package Com.kylegoslin;
+
+public class ExampleUsage_1 {
+	
+	/***
+	 * 
+	 * Author: Tomasz Piechota 
+	 * Project: Java - Semester 4 - Main Assignment
+	 * 
+	 ***/
+	
+	public ExampleUsage_1(){	
+
+		/*** Logger StartUp - BEGIN ***/
+		// This line has to be added to testing class for the Logger to work correctly!
+		StartUp.config(this.getClass().getName().toString(), this.getClass().getPackage().getName().toString());
+		/*** Logger StartUp - END ***/
+		
+		exampleMethod();
+		
+	}
+	
+	public void exampleMethod(){
+		
+		System.out.println("Hello World!");
+		
+	}
+	
+	public static void main (String [] args){
+		
+		new ExampleUsage_1(); // Main
+		
+	}
+
+}
