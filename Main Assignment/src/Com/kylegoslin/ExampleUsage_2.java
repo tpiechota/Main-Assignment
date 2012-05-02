180e735d973033648bbcba7ed9e60bc0a1cf4947
Tomasz Piechota <piechota.tomasz@gmail.com>
2012-05-02 08:00:24 +0000

    ExampleUsage_2 - another example usage

---
+++ Main Assignment/src/Com/kylegoslin/ExampleUsage_2.java
@@ -0,0 +1,127 @@
+package Com.kylegoslin;
+
+	import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+
+public class ExampleUsage_2 {
+	
+	boolean testOK;
+	boolean isNumber;
+	String fName;
+
+
+	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	
+	public ExampleUsage_2(){
+		
+		/*** Logger StartUp - BEGIN ***/
+		// This line has to be added to testing class for the Logger to work correctly!
+		StartUp.config(this.getClass().getName().toString(), this.getClass().getPackage().getName().toString());
+		/*** Logger StartUp - END ***/
+		
+		try{
+			
+			enterName(); 
+			
+		}catch (Error ex){
+			
+			testOK =  false;
+
+			LogHandler.logger.log(Level.WARNING, "trouble processing input", ex);
+			
+		}
+		
+		LogHandler.logger.fine("done");
+		
+		System.out.println("First Name.......: " + fName);
+		
+		System.exit(0);
+
+		
+	}
+	
+	// Just a simple User input command
+	public void enterName(){
+		
+		do{// First Name BEGIN.
+			
+			System.out.print("ENTER FIRST NAME.......: ");
+			
+			testOK =  true;
+			isNumber = true;
+			
+			try{
+				
+				fName = reader.readLine();
+				
+		    	// Validates if FirstName is a number.
+				try{
+		    		
+		    		Integer.parseInt(fName);
+		    		
+		    		isNumber = true;
+		    		
+		    	}catch( Exception e){
+
+		    		isNumber = false;
+
+		    	}
+		    	
+				//Checks if the user entered any value and it is at least 3 characters long.
+				if(fName.isEmpty() || fName.length() <= 3){
+				
+					testOK = false;
+					
+					LogHandler.logger.log(Level.SEVERE, "User input too short (min. 3 characters)");
+
+					System.out.println("*** ERROR! TRY AGAIN (min. 3 characters) ***");
+					System.out.println();
+				
+				}else if(isNumber == true){// If the input is a number testOK = false.
+					
+					testOK = false;
+					
+					LogHandler.logger.log(Level.WARNING, "User entered Integer value instead String/Char.");
+					
+					System.out.println("*** ERROR! NO NUMBERS ALLOWED! TRY AGAIN ***");
+					System.out.println();
+					
+				}else if(fName.contains(" ")){// If the input contains spaces " " then testOK = false.
+					
+					testOK = false;
+					
+					LogHandler.logger.log(Level.WARNING, "User entered blank characters.");
+					
+					System.out.println("*** ERROR! NO SPACES ALLOWED! TRY AGAIN ***");
+					System.out.println();
+					
+				}else{
+					
+					testOK = true;
+					
+				}
+				
+			}catch(Exception e){
+				
+				testOK = false;
+				
+				LogHandler.logger.log(Level.INFO, "trouble processing input", e);
+				
+				System.out.println("*** ERROR! TRY AGAIN ***");
+				System.out.println();
+				
+			}
+			
+		}while(!testOK);// First Name END.
+		
+		
+	}
+	
+	public static void main (String [] args){
+
+		new ExampleUsage_2();
+		
+	}
+	
+}
