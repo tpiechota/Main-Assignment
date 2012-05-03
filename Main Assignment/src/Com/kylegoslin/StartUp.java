e092acfc808c4bf5fe368bd565f9e0320860e4d4
Tomasz Piechota <piechota.tomasz@gmail.com>
2012-05-03 18:36:53 +0000

    Touchups

--- Main Assignment/src/Com/kylegoslin/StartUp.java
+++ Main Assignment/src/Com/kylegoslin/StartUp.java
@@ -47,19 +47,19 @@ public class StartUp {
 		// Set logger level and messages for each level
 		LogHandler.logger.setLevel(Level.ALL);
 		
-		LogHandler.logger.severe("Severe Message");
-		LogHandler.logger.warning("Warning Message");
 		LogHandler.logger.info("Info Message");
 		LogHandler.logger.fine("Fine Message");
 		LogHandler.logger.finer("Finer Message");
 		LogHandler.logger.finest("Finest Message");
+		LogHandler.logger.severe("Severe Message");
+		LogHandler.logger.warning("Warning Message");
 		
-		LogHandler.logger.log(Level.SEVERE, "This is a severe level message!");
-		LogHandler.logger.log(Level.WARNING, "This is a warning level message!");
 		LogHandler.logger.log(Level.INFO, "This is a info level message!");
 		LogHandler.logger.log(Level.FINE, "This is a fine level message!");
 		LogHandler.logger.log(Level.FINER, "This is a finer level message!");
 		LogHandler.logger.log(Level.FINEST, "This is a finest level message!");
+		LogHandler.logger.log(Level.SEVERE, "This is a severe level message!");
+		LogHandler.logger.log(Level.WARNING, "This is a warning level message!");
 		
 	}
 	
