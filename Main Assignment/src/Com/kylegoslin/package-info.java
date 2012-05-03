9686f922924a273d90f12d370769c9a36fb79b12
Tomasz Piechota <piechota.tomasz@gmail.com>
2012-05-03 10:30:55 +0000

    Update on package info

--- Main Assignment/src/Com/kylegoslin/package-info.java
+++ Main Assignment/src/Com/kylegoslin/package-info.java
@@ -3,7 +3,7 @@
  * <h2 style="text-align: center;">Error Logging / Warning Logging Tool</h2>
  * <h3 style="text-align: center;">Advanced Programming - Main Assignment</h3>
  * <br />
- * <p>Provides the classes necessary to create the Logging Tool that can be used with other applications.</p>
+ * <p>Provides the classes necessary to create the Logging Tool, that can be used with other applications.</p>
  * 
  * @since 1.0
  * 
