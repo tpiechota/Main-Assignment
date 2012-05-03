41dd9633b2cf4a49d0025e8350fc57b13d887480
Tomasz Piechota <piechota.tomasz@gmail.com>
2012-05-03 11:08:42 +0000

    Javadoc update

--- Main Assignment/src/Com/kylegoslin/StartUp.java
+++ Main Assignment/src/Com/kylegoslin/StartUp.java
@@ -1,11 +1,14 @@
 package Com.kylegoslin;
 
+
 import java.io.IOException;
 import java.util.logging.Level;
 
+
 /**
  * <p>The StartUp class starts up / initializes logging application.</p>
- * <p>This is part of the error logging / warning tool that can be used with any application.</p>
+ * 
+ * <p>This is part of the error logging / warning logging tool that can be used with any application.</p>
  * @author Tomasz Piechota
  * @version 0.1 Build 0001 May 03, 2012.
  *
@@ -16,7 +19,12 @@ public class StartUp {
 	static String className;
 	static String packageName;
 	
-	// This method takes class name and package name from testing class
+	/**
+	 * This method takes class name and package name from Logger Initiator placed in testing class.
+	 *  
+	 * @param cn className - stores class name of the testing class
+	 * @param pn packageName - stores package name of the testing class
+	 */
 	public static void config(String cn, String pn){
 		
 		className = cn;
@@ -56,18 +64,35 @@ public class StartUp {
 	}
 	
 	// Getters and Setters
+	
+	/**
+	 * Returns class name of the testing class
+	 * @return className - testing class name
+	 */
 	public String getClassName() {
 		return className;
 	}
 
+	/**
+	 * Sets className variable with the name provided by Logger Initiator
+	 * @param className testing class name
+	 */
 	public void setClassName(String className) {
 		StartUp.className = className;
 	}
 
+	/**
+	 * Returns package name of the testing class
+	 * @return packageName - testing class package name
+	 */
 	public String getPackageName() {
 		return packageName;
 	}
 
+	/**
+	 * Sets packageName variable with the name provided by Logger Initiator
+	 * @param packageName testing class package name
+	 */
 	public static void setPackageName(String packageName) {
 		StartUp.packageName = packageName;
 	}
