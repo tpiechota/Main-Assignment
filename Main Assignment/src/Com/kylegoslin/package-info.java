164a4cb9a39e1391e6f53c8479756e2d6255aac6
Tomasz Piechota <piechota.tomasz@gmail.com>
2012-05-03 10:23:27 +0000

    Package info update

--- Main Assignment/src/Com/kylegoslin/package-info.java
+++ Main Assignment/src/Com/kylegoslin/package-info.java
@@ -3,13 +3,8 @@
  * <h2 style="text-align: center;">Error Logging / Warning Logging Tool</h2>
  * <h3 style="text-align: center;">Advanced Programming - Main Assignment</h3>
  * <br />
- * <p>Provides the classes necessary to create the Logging Tool to communicate with its applet context.</p>
+ * <p>Provides the classes necessary to create the Logging Tool that can be used with other applications.</p>
  * 
- * <p>The applet framework involves two entities: 
- * the applet and the applet context. An applet is an embeddable window (see the 
- * {@link java.awt.Panel} class) with a few extra methods that the applet context 
- * can use to initialize, start, and stop the applet.</p>
- *
  * @since 1.0
  * 
  */
